b19b352aa06da8823e69ca937c5c614f6dd3d306
antortjim <ntoniohu@gmail.com>
2024-11-29 16:58:28 +0000

    Update figures and notebooks

--- combine_figures.sh
+++ combine_figures.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-FOLDER="./figures/2024-10-27"
+FOLDER="./figures/paper/"
 for i in `seq 1 5`
 do
   ~/opt/Inkscapev1.4.AppImage ${FOLDER}/Fig${i}/Figure_${i}m.svg -d 200 -C --batch-process --export-type=pdf --export-filename=${FOLDER}/Fig${i}/Figure_${i}m.pdf
